0397efcb327309f8e8beccd00acca8e5d1729793
gaarahan <gaarahan@foxmail.com>
2020-08-12 19:59:31 +0800

    [Gaarahan] exercise 1 completed

--- src/exercise-1/components/App.js
+++ src/exercise-1/components/App.js
@@ -1,23 +1,39 @@
 import React, {Component} from 'react';
-import './App.less';
 import Home from "./Home/Home";
 import MyProfile from "./MyProfile/MyProfile";
 import AboutUs from "./AboutUs/AboutUs";
-import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import {BrowserRouter as Router, Route, NavLink} from "react-router-dom";
+import './App.scss';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      curPath: '/'
+    }
+  }
   render() {
     return (
-      <Router className="app">
-        <header className="header">
-          <Link to="/" >Home</Link>
-          <Link to="/my-profile" >My Profile</Link>
-          <Link to="/about-us" >About Us</Link>
-        </header>
-        <Route exact path="/" component={Home}/>
-        <Route path="/my-profile" component={MyProfile}/>
-        <Route path="/about-us" component={AboutUs}/>
-      </Router>
+        <Router className="app">
+          <header className="page-header">
+            <NavLink
+                exact
+                to="/"
+                activeClassName="active"
+            >Home</NavLink>
+            <NavLink
+                to="/my-profile"
+                activeClassName="active"
+            >My Profile</NavLink>
+            <NavLink
+                to="/about-us"
+                activeClassName="active"
+            >About Us</NavLink>
+          </header>
+          <Route exact path="/" component={Home}/>
+          <Route path="/my-profile" component={MyProfile}/>
+          <Route path="/about-us" component={AboutUs}/>
+        </Router>
     );
   }
 }
